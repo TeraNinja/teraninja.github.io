69914f897f93471a2e3be0d2fb4468475be7822d
Valky <37550438+ItsValky@users.noreply.github.com>
2019-02-22 14:29:06 +0100

    u f c n
    updates sites with new icon
    fixed wrapper for valk-player
    changed icon for valk-player
    new video for skill rotation

--- scripts/player.js
+++ scripts/player.js
@@ -1,11 +1,29 @@
 
-// play pause button
-$('.valk-design').parent().click(function () {
-    if($(this).children(".valk-design").get(0).paused){
-        $(this).children(".valk-design").get(0).play();
+// fade in/out player icon
+$('.valk-wrapper').click(function () {
+    
+    if ($(this).children(".valk-player").get(0).paused) {
+
+        // fade out icon and start video
+        $(this).children(".valk-player").get(0).play();
         $(this).children(".playpause").fadeOut();
-      }else{
-        $(this).children(".valk-design").get(0).pause();
+
+        // fade in icon when video ended
+        $(this).children(".valk-player").get(0).addEventListener('ended',function(){
+            $('.valk-wrapper').children(".playpause").fadeIn();
+        });
+
+    } 
+    
+    else {
+      
+        // fade in icon when video paused
+        $(this).children(".valk-player").get(0).pause();
         $(this).children(".playpause").fadeIn();
-      }
+
+    }
+
+
 });
+
+
